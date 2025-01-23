0ae89004ce07d6d57f66c925f7605a4aeb4e4aa3
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-01-23 10:25:33 -0500

    Add .zsh/functions.zsh

---
+++ dot_zsh/functions.zsh
@@ -0,0 +1,17 @@
+# Makes a directory and changes to it.
+function mkdcd {
+  [[ -n "$1" ]] && mkdir -p "$1" && builtin cd "$1"
+}
+
+# Changes to a directory and lists its contents.
+function cdls {
+  builtin cd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+
+# Deletes .DS_Store and __MACOSX directories.
+function osx-rm-dir-metadata {
+  find "${@:-$PWD}" \( \
+    -type f -name '.DS_Store' -o \
+    -type d -name '__MACOSX' \
+  \) -print0 | xargs -0 rm -rf
+}

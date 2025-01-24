cb3181f5be0883a18d67b995952b78bb0231d8ed
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-01-23 22:30:41 -0500

    Update .zsh/plugins/zsh-completions.zsh

--- dot_zsh/plugins/zsh-completions.zsh
+++ dot_zsh/plugins/zsh-completions.zsh
@@ -3,3 +3,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 # TODO: Make the colours match the colours in eza.
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*' menu no
+
+# Must be loaded after zsh-completions.
+autoload -U compinit && compinit

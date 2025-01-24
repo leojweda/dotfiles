804e08c7ef45d440ecc705e12e935c3cba6493bc
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-01-23 22:54:47 -0500

    Add .zsh/plugins/zsh-auto-notify.zsh

---
+++ dot_zsh/plugins/zsh-auto-notify.zsh
@@ -0,0 +1,6 @@
+zinit light 'MichaelAquilina/zsh-auto-notify'
+AUTO_NOTIFY_EXPIRE_TIME=7000
+AUTO_NOTIFY_THRESHOLD=10
+AUTO_NOTIFY_IGNORE+=(
+  'docker', 'htop', 'less', 'man', 'more', 'nano', 'nvim', 'ssh', 'tmux', 'top', 'vim', 'watch'
+)

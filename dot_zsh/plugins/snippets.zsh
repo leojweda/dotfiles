640e32bf1de1b899dddd42891b06f5925553b71e
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-01-23 23:00:45 -0500

    Add .zsh/plugins/snippets.zsh

---
+++ dot_zsh/plugins/snippets.zsh
@@ -0,0 +1,7 @@
+zinit snippet https://raw.githubusercontent.com/junegunn/fzf-git.sh/main/fzf-git.sh
+zinit snippet OMZP::colored-man-pages
+zinit snippet OMZP::encode64
+zinit snippet OMZP::man
+zinit snippet OMZP::thefuck
+zinit snippet OMZP::tldr
+# zinit snippet OMZP::vi-mode

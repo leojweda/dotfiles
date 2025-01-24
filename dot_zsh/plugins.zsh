a47a9f0dc44650c0c33628f6145f70e321312ec0
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-01-23 22:54:59 -0500

    Update .zsh/plugins.zsh

--- dot_zsh/plugins.zsh
+++ dot_zsh/plugins.zsh
@@ -3,9 +3,12 @@ export PLUGINS_DIR="$HOME/.zsh/plugins"
 source "$PLUGINS_DIR/zsh-completions.zsh"
 # Must be loaded after zsh-completions.
 source "$PLUGINS_DIR/fzf-tab.zsh"
+
 # Must be loaded before history-substring-search, autosuggestions, and the prompt.
 source "$PLUGINS_DIR/zsh-syntax-highlighting.zsh"
 # Must be loaded after syntax-highlighting, but before augosuggestions and the prompt.
 source "$PLUGINS_DIR/zsh-history-substring-search.zsh"
 # Must be loaded after syntax-highlighting and history-substring-search, but before the prompt.
 source "$PLUGINS_DIR/zsh-autosuggestions.zsh"
+
+source "$PLUGINS_DIR/zsh-auto-notify.zsh"

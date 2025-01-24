7306835170404c61b32d00f46633636d45ec620d
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-01-23 22:38:16 -0500

    Update .zsh/plugins.zsh

--- dot_zsh/plugins.zsh
+++ dot_zsh/plugins.zsh
@@ -3,3 +3,5 @@ export PLUGINS_DIR="$HOME/.zsh/plugins"
 source "$PLUGINS_DIR/zsh-completions.zsh"
 # Must be loaded after zsh-completions.
 source "$PLUGINS_DIR/fzf-tab.zsh"
+# Must be loaded before history-substring-search, autosuggestions, and the prompt.
+source "$PLUGINS_DIR/zsh-syntax-highlighting.zsh"

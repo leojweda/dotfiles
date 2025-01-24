efe4590e87731a9bd9754a764d4557c17dba07f6
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-01-24 10:40:45 -0500

    Update .zsh/prompt.zsh

--- dot_zsh/prompt.zsh
+++ dot_zsh/prompt.zsh
@@ -2,3 +2,7 @@
 if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
   eval "$(oh-my-posh init zsh --config ${XDG_CONFIG_HOME}/ohmyposh/base.json)"
 fi
+
+function set_poshcontext() {
+    export BG_JOBS=$(jobs|wc -l|xargs)
+}

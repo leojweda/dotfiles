1efec91124e389df981758519349edad06faafb7
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-01-29 09:49:00 -0500

    Updated install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ fi
 
 echo '📦  Installing dependencies' >&2 && \
 brew update && \
-brew bundle
+brew bundle --file=~/.config/brew/Brewfile
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"

793f9705eabebf9d49b1cec761ba9b5bdfb85e83
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-02-01 13:32:56 -0500

    Add .config/tmux/update_window_indices.sh

---
+++ dot_config/tmux/executable_update_window_indices.sh
@@ -0,0 +1,9 @@
+#!/bin/bash  # or #!/bin/zsh if using Zsh
+
+# Get the current window index
+current_index=$(tmux display-message -p -F "#{window_index}")
+
+# Compute the previous and next window indices
+previous_index=$((current_index - 1))
+
+tmux set -g @TMUX_PREVIOUS_WINDOW_INDEX "$previous_index"

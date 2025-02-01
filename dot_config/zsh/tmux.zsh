9013621949b028a1a7e7f7026bef5abf810d1925
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-02-01 18:31:21 -0500

    Update .config/zsh/tmux.zsh

--- dot_config/zsh/tmux.zsh
+++ dot_config/zsh/tmux.zsh
@@ -1,3 +1,3 @@
-if [[ -z "$TMUX" && -z "$INSIDE_EMACS" && -z "$EMACS" && -z "$VIM" && -z "$INTELLIJ_ENVIRONMENT_READER" ]]; then
+if [[ -z "$TMUX" && -z "$INSIDE_EMACS" && -z "$EMACS" && -z "$VIM" && -z "$INTELLIJ_ENVIRONMENT_READER" && "$TERM_PROGRAM" != "vscode" ]]; then
   tmux attach -t default || tmux new -s default
 fi

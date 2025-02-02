d1b62a7a9daffc6a7b198edbc983069040cd7043
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-02-01 21:54:57 -0500

    Update .config/zsh/plugins/zsh-auto-notify.zsh

--- dot_config/zsh/plugins/zsh-auto-notify.zsh
+++ dot_config/zsh/plugins/zsh-auto-notify.zsh
@@ -1,7 +1,9 @@
-zinit light 'MichaelAquilina/zsh-auto-notify'
+if [[ $OSTYPE == darwin* ]]; then
+  zinit light 'MichaelAquilina/zsh-auto-notify'
 
-AUTO_NOTIFY_EXPIRE_TIME=7000
-AUTO_NOTIFY_THRESHOLD=10
-AUTO_NOTIFY_IGNORE+=(
-  'docker', 'htop', 'less', 'man', 'more', 'nano', 'nvim', 'ssh', 'tmux', 'top', 'vim', 'watch'
-)
+  AUTO_NOTIFY_EXPIRE_TIME=7000
+  AUTO_NOTIFY_THRESHOLD=10
+  AUTO_NOTIFY_IGNORE+=(
+    'docker', 'htop', 'less', 'man', 'more', 'nano', 'nvim', 'ssh', 'tmux', 'top', 'vim', 'watch'
+  )
+fi

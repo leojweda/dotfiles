75dc7e63956aa1d0b5e298db526feb9eec343145
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-02-07 21:06:57 -0500

    Update .config/zsh/plugins/fzf-tab.zsh

--- dot_config/zsh/plugins/fzf-tab.zsh
+++ dot_config/zsh/plugins/fzf-tab.zsh
@@ -1,4 +1,7 @@
-# Must be loaded after zsh-completions.
+# Must be loaded after zsh-completions, but before zsh-syntax-highlighting and zsh-autosuggestions.
 zinit light Aloxaf/fzf-tab
 
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+
+fzf_default_opts="$(tr '\n' ' ' < "$FZF_DEFAULT_OPTS_FILE")"
+zstyle ':fzf-tab:*' fzf-flags ${(s: :)fzf_default_opts}

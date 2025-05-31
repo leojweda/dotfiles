2b5e91d7bff36ca7e066c5f55da61c0f48d88398
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-05-31 12:54:18 -0400

    Add .config/zsh/git.zsh

---
+++ dot_config/zsh/git.zsh
@@ -0,0 +1,68 @@
+alias g='git'
+
+alias ga='git add'
+alias gaa='git add --all'
+
+alias gb='git branch'
+
+alias gbs='git bisect'
+alias gbsb='git bisect bad'
+alias gbsg='git bisect good'
+alias gbsn='git bisect new'
+alias gbso='git bisect old'
+alias gbsr='git bisect reset'
+alias gbss='git bisect start'
+
+alias gc='git commit'
+alias gcm='git commit -m'
+
+alias gco='git checkout'
+alias gcob='git checkout -b'
+alias gcoB='git checkout -B'
+
+alias gcp='git cherry-pick'
+alias gcpa='git cherry-pick --abort'
+alias gcpc='git cherry-pick --continue'
+
+alias gd='git diff'
+
+alias gl='git log'
+alias glg='git log --graph'
+
+alias gm='git merge'
+alias gma='git merge --abort'
+alias gmc='git merge --continue'
+alias gms="git merge --squash"
+alias gmt='git mergetool'
+
+alias gpl='git pull'
+
+alias gp='git push'
+
+alias grb='git rebase'
+alias grba='git rebase --abort'
+alias grbc='git rebase --continue'
+alias grbi='git rebase --interactive'
+alias grbo='git rebase --onto'
+alias grbs='git rebase --skip'
+
+alias gr='git remote'
+alias gra='git remote add'
+alias grrm='git remote remove'
+alias grmv='git remote rename'
+alias grup='git remote update'
+
+alias grs='git restore'
+alias grss='git restore --source'
+alias grst='git restore --staged'
+
+alias gsh='git show'
+
+alias gst='git stash --all'
+alias gstaa='git stash apply'
+alias gstc='git stash clear'
+alias gstd='git stash drop'
+alias gstl='git stash list'
+alias gstp='git stash pop'
+
+alias gst='git status'

0326a192894a45f7b4cbdd20e7fd6bdd42d6581c
Leo Jweda <792247+leojweda@users.noreply.github.com>
2025-10-04 12:04:07 -0400

    Update .config/zsh/homebrew.zsh

--- dot_config/zsh/homebrew.zsh
+++ dot_config/zsh/homebrew.zsh
@@ -9,7 +9,7 @@ if [[ $OSTYPE == linux* ]]; then
 fi
 
 # Homebrew Command Not Found
-HB_CNF_HANDLER="$(brew --repository)/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
-if [ -f "$HB_CNF_HANDLER" ]; then
-  source "$HB_CNF_HANDLER";
+HOMEBREW_COMMAND_NOT_FOUND_HANDLER="$(brew --repository)/Library/Homebrew/command-not-found/handler.sh"
+if [ -f "$HOMEBREW_COMMAND_NOT_FOUND_HANDLER" ]; then
+  source "$HOMEBREW_COMMAND_NOT_FOUND_HANDLER";
 fi

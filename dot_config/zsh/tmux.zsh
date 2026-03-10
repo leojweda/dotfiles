af0c70bd18c93b0d0f662b24b97a5f945d70c29f
Leo Jweda <792247+leojweda@users.noreply.github.com>
2026-03-10 11:00:50 -0400

    Remove .config/zsh/tmux.zsh

--- dot_config/zsh/tmux.zsh
+++
@@ -1,3 +0,0 @@
-if [[ -z "$TMUX" && -z "$EMACS" && -z "$INSIDE_EMACS" && -z "$VIM" && -z "$VSCODE_RESOLVING_ENVIRONMENT" && "$TERM_PROGRAM" != "vscode" && "$TERMINAL_EMULATOR" != "JetBrains-JediTerm" ]]; then
-  tmux attach -t default || tmux new -s default
-fi

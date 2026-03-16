bb6a5bad02264b01f5fae9873daaebcd3aeb17a8
Leo Jweda <792247+leojweda@users.noreply.github.com>
2026-03-16 12:14:55 -0400

    Remove .config/zsh/gh.zsh

--- dot_config/zsh/gh.zsh
+++
@@ -1,6 +0,0 @@
-if ! gh extension list 2>/dev/null | grep -q 'github/gh-copilot'; then
-    echo "Installing GitHub CLI Copilot extension..."
-    gh extension install github/gh-copilot
-fi
-
-eval "$(gh copilot alias -- zsh)"
